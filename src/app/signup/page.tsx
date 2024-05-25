60d88cf4dd85848e9a8dab541fa65a2712fc0a22
bhargavshirin <bnalamat@gitam.in>
2024-05-25 23:46:35 +0530

    ff

--- src/app/signup/page.tsx
+++ src/app/signup/page.tsx
@@ -1,7 +1,8 @@
 'use client';
 import React, { useState, useEffect, ChangeEvent } from 'react';
 import { getAuth, createUserWithEmailAndPassword, Auth } from 'firebase/auth';
-import { app } from '../firebase/Config';
+import {doc, getDoc, setDoc } from 'firebase/firestore';
+import { app,db } from '../firebase/Config';
 import { useRouter } from 'next/navigation';
 import Link from 'next/link';
 import Spinner from '../components/spinner';
@@ -9,6 +10,7 @@ import Spinner from '../components/spinner';
 interface LoginPageState {
   email: string;
   password: string;
+  name: string;
   error: string;
   success: string;
   authInitialized: boolean;
@@ -19,6 +21,7 @@ const SignupPage: React.FC = () => {
   const [state, setState] = useState<LoginPageState>({
     email: '',
     password: '',
+    name: '',
     error: '',
     success: '',
     authInitialized: false,
@@ -51,26 +54,45 @@ const SignupPage: React.FC = () => {
     }));
   };
 
+  const handleNameChange = (e: ChangeEvent<HTMLInputElement>) => {
+    setState((prevState) => ({
+      ...prevState,
+      name: e.target.value,
+    }));
+  };
+
   const handleSignUp = async () => {
     setLoading(true);
-    const { email, password, auth } = state;
+    const { email, password, name, auth } = state;
+    // const db = getFirestore(app);
     try {
       if (auth) {
-        await createUserWithEmailAndPassword(auth, email, password);
-        setState((prevState) => ({
-          ...prevState,
-          success: 'Account created successfully',
-          email: '',
-          password: '',
-        }));
+        const userDoc = doc(db, 'users', email);
+        const userSnapshot = await getDoc(userDoc);
+        if (!userSnapshot.exists()) {
+          await createUserWithEmailAndPassword(auth, email, password);
+          await setDoc(userDoc, { email, name });
+          setState((prevState) => ({
+            ...prevState,
+            success: 'Account created successfully',
+            email: '',
+            password: '',
+            name: '',
+          }));
+        } else {
+          setState((prevState) => ({
+            ...prevState,
+            error: 'Email already exists',
+          }));
+        }
       }
     } catch (error) {
       console.error('Error signing up with email and password', error);
       setState((prevState) => ({
         ...prevState,
-        error: 'Email already exists / Error signing up',
+        error: 'Error signing up / Emails exists',
       }));
-    }finally {  
+    } finally {
       setLoading(false);
     }
   };
@@ -81,9 +103,23 @@ const SignupPage: React.FC = () => {
 
   return (
     <div className="flex items-center justify-center h-screen">
-      <div className="rounded-3xl w-full max-w-md p-6 bg-white  shadow-md">
+      <div className="rounded-3xl w-full max-w-md p-6 bg-white shadow-md">
         <h1 className="text-3xl font-bold mb-6 text-black">Signup</h1>
         <form className="mb-4">
+          <div className="mb-4">
+            <label htmlFor="name" className="block text-gray-700 text-sm font-bold mb-2">
+              Name
+            </label>
+            <input
+              type="text"
+              id="name"
+              name="name"
+              value={state.name}
+              onChange={handleNameChange}
+              className="w-full border rounded-md py-2 px-3"
+              required
+            />
+          </div>
           <div className="mb-4">
             <label htmlFor="email" className="block text-gray-700 text-sm font-bold mb-2">
               Email
@@ -116,7 +152,7 @@ const SignupPage: React.FC = () => {
           {!state.error && state.success && <div className="text-green-500 text-sm mb-2">{state.success}</div>}
         </form>
         <Link href="/login">
-          <p className="text-gray-600 text-sm mb-2 hover:text-underline">Have an account, Login</p>
+          <p className="text-gray-600 text-sm mb-2 hover:underline">Have an account? Login</p>
         </Link>
         <button
           onClick={handleSignUp}
